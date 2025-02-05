f449e908973dbe97fd7cae0757943cd09cb64c59
661111 <xiaoxin104@outlook.com>
2025-02-05 13:51:25 +0800

    修改essay代码

--- source/js/bbtalk.js
+++ source/js/bbtalk.js
@@ -1,11 +1,19 @@
-/* 首页bber JS */
-if (document.querySelector('#bber-talk')) {
-    var swiper = new Swiper('.swiper-container', {
-      direction: 'vertical', // 垂直切换选项
-      loop: true,
-      autoplay: {
-      delay: 3000,
-      pauseOnMouseEnter: true
-    },
+function whenDOMReady() {
+	initEssay();
+};
+
+whenDOMReady()
+document.addEventListener("pjax:complete", whenDOMReady)
+
+function initEssay() {
+  if (document.querySelector('#essay-mini')) {
+    let swiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            pauseOnMouseEnter: true
+        },
     });
   }
+} // Swiper轮播 - 即刻mini
--- source/js/essay.js
+++ source/js/essay.js
@@ -1,129 +1,69 @@
-var percentFlag = false; // 节流阀
-function essayScroll() {
-    let a = document.documentElement.scrollTop || window.pageYOffset; // 当前滚动位置
-    const waterfallResult = a % document.documentElement.clientHeight; // 滚动的视口高度余数
-
-    if (
-        !percentFlag &&
-        waterfallResult + 100 >= document.documentElement.clientHeight &&
-        document.querySelector("#waterfall")
-    ) {
-        setTimeout(() => {
-            waterfall("#waterfall");
-        }, 500);
-    } else {
-        setTimeout(() => {
-            document.querySelector("#waterfall") && waterfall("#waterfall");
-        }, 500);
-    }
-
-    const r = window.scrollY + document.documentElement.clientHeight;
-    let p = document.getElementById("post-comment") || document.getElementById("footer");
-
-    (p.offsetTop + p.offsetHeight / 2 < r || 90 < result) && (percentFlag = true);
-}
-function replaceAll(e, n, t) {
-    return e.split(n).join(t);
-}
-var june = {
-    diffDate: function (d, more = false) {
-        const dateNow = new Date();
-        const datePost = new Date(d);
-        const dateDiff = dateNow.getTime() - datePost.getTime();
-        const minute = 1000 * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-        const month = day * 30;
-
-        let result;
-        if (more) {
-            const monthCount = dateDiff / month;
-            const dayCount = dateDiff / day;
-            const hourCount = dateDiff / hour;
-            const minuteCount = dateDiff / minute;
-
-            if (monthCount >= 1) {
-                result = datePost.toLocaleDateString().replace(/\//g, "-");
-            } else if (dayCount >= 1) {
-                result = parseInt(dayCount) + " " + GLOBAL_CONFIG.date_suffix.day;
-            } else if (hourCount >= 1) {
-                result = parseInt(hourCount) + " " + GLOBAL_CONFIG.date_suffix.hour;
-            } else if (minuteCount >= 1) {
-                result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.date_suffix.min;
-            } else {
-                result = GLOBAL_CONFIG.date_suffix.just;
-            }
+function whenDOMReady() {
+    if (location.pathname == '/essay/') document.addEventListener('DOMContentLoaded', function () {setTimeout(() => { changeTime(), btf.loadLightbox(document.querySelectorAll('#icat-bber img')), window.lazyLoadInstance && window.lazyLoadInstance.update(), reflashWaterFall();}, 300)})
+  }
+  whenDOMReady()
+  document.addEventListener("pjax:complete", whenDOMReady)
+  
+  // 适配pjax
+  
+  window.onresize = () => {
+    waterfall('#waterfall');
+  };
+  
+  // 自适应
+  
+  function timeDiff(timeObj, today) => {
+    const timeObjUTC = Date.UTC(timeObj.getFullYear(), timeObj.getMonth(), timeObj.getDate());
+    const todayUTC = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+  
+    const timeDiff = todayUTC - timeObjUTC;
+    return Math.floor(timeDiff / (1000 * 3600 * 24));
+  }
+  function changeTime() {
+    const timeElements = Array.from(document.getElementsByTagName("time"));
+    const currentDate = new Date();
+  
+    timeElements.forEach(timeElement => {
+      const datetime = timeElement.getAttribute("datetime");
+      const timeObj = new Date(datetime);
+      const daysDiff = timeDiff(timeObj, currentDate);
+  
+      let timeString;
+      if (daysDiff === 0) {
+        timeString = `最近`;
+      } else if (daysDiff === 1) {
+        timeString = `昨天`;
+      } else if (daysDiff === 2) {
+        timeString = `前天`;
+      } else if (daysDiff <= 7) {
+        timeString = `${daysDiff}天前`;
+      } else {
+        if (timeObj.getFullYear() !== currentDate.getFullYear()) {
+          timeString = `${timeObj.getFullYear()}/${timeObj.getMonth() + 1}/${timeObj.getDate()}`;
         } else {
-            result = parseInt(dateDiff / day);
+          timeString = `${timeObj.getMonth() + 1}/${timeObj.getDate()}`;
         }
-        return result;
-    },
-    changeTimeInEssay: function () {
-        document.querySelector("#bber") &&
-        document.querySelectorAll("#bber time").forEach(function (e) {
-            var t = e,
-                datetime = t.getAttribute("datetime");
-            (t.innerText = june.diffDate(datetime, true)), (t.style.display = "inline");
-        });
-    },
-    reflashEssayWaterFall: function () {
-        document.querySelector("#waterfall") &&
-        setTimeout(function () {
-            waterfall("#waterfall");
-            document.getElementById("waterfall").classList.add("show");
-        }, 500);
-    },
-    commentText: function (txt) {
-        const postCommentDom = document.querySelector("#post-comment");
-        var domTop = postCommentDom.offsetTop;
-        window.scrollTo(0, domTop - 80);
-        if (txt == "undefined" || txt == "null") txt = "好棒！";
-        function setText() {
-            setTimeout(() => {
-                var input = document.getElementsByClassName("el-textarea__inner")[0];
-                if (!input) setText();
-                let evt = document.createEvent("HTMLEvents");
-                evt.initEvent("input", true, true);
-                let inputValue = replaceAll(txt, "\n", "\n> ");
-                input.value = "> " + inputValue + "\n\n";
-                input.dispatchEvent(evt);
-                input.focus();
-                input.setSelectionRange(-1, -1);
-                if (document.getElementById("comment-tips")) {
-                    document.getElementById("comment-tips").classList.add("show");
-                }
-            }, 100);
-        }
-        setText();
-    },
-    initIndexEssay: function () {
-        setTimeout(() => {
-            let essay_bar_swiper = new Swiper(".essay_bar_swiper_container", {
-                passiveListeners: true,
-                direction: "vertical",
-                loop: true,
-                autoplay: {
-                    disableOnInteraction: true,
-                    delay: 3000,
-                },
-                mousewheel: true,
-            });
-
-            let essay_bar_comtainer = document.getElementById("bbtalk");
-            if (essay_bar_comtainer !== null) {
-                essay_bar_comtainer.onmouseenter = function () {
-                    essay_bar_swiper.autoplay.stop();
-                };
-                essay_bar_comtainer.onmouseleave = function () {
-                    essay_bar_swiper.autoplay.start();
-                };
-            }
-        }, 100);
-    },
-};
-
-june.initIndexEssay();
-june.changeTimeInEssay();
-june.reflashEssayWaterFall();
-
-window.addEventListener("scroll", essayScroll);
+      }
+      timeElement.textContent = timeString;
+    });
+  }
+  function reflashWaterFall() {
+    document.querySelector("#waterfall") &&
+      setTimeout(function() {
+        waterfall("#waterfall");
+        document.getElementById("waterfall")
+          .classList.add("show");
+      }, 500);
+  } // 加载显示 - 即刻短文
+  function commentText(txt) {
+    const inputs = ["#wl-edit", ".el-textarea__inner"];
+    for (let i = 0; i < inputs.length; i++) {
+      let el = document.querySelector(inputs[i]);
+      if (el != null) {
+        el.dispatchEvent(new Event('input', { bubble: true, cancelable: true }));
+        el.value = '> ' + txt.replace(/\n/g, '\n> ') + '\n\n';
+        el.focus();
+        el.setSelectionRange(-1, -1);
+      }
+    }
+  } // 引用评论跳转 - 即刻短文 
