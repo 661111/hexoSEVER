1a4451997c0c06d1208e28d3a8e17b714b142159
661111 <xiaoxin104@outlook.com>
2025-02-07 21:30:27 +0800

    更新

--- source/js/sxiaohe.js
+++ source/js/sxiaohe.js
@@ -27,3 +27,23 @@ $(".topGroup").hover((function() {}
         document.getElementById("todayCard").style.zIndex = 1
     }
 ))
+
+function whenDOMReady() {
+	initEssay();
+};
+
+whenDOMReady()
+document.addEventListener("pjax:complete", whenDOMReady)
+
+function initEssay() {
+  if (document.querySelector('#essay-mini')) {
+    let swiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            pauseOnMouseEnter: true
+        },
+    });
+  }
+} // Swiper轮播 - 即刻mini
