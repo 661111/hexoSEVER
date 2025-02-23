d0e3538d5faa1bb7866e5ec0c4efdf6029f07749
661111 <xiaoxin104@outlook.com>
2025-02-23 14:38:03 +0800

    优化添加内容

--- source/js/sxiaohe.js
+++ source/js/sxiaohe.js
@@ -1,33 +1,3 @@
-/* switchDarkMode JS */
-function switchDarkMode() { // Switch Between Light And Dark Mode
-    const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
-    if (nowMode === 'light') {
-      activateDarkMode()
-      saveToLocal.set('theme', 'dark', 2)
-      GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
-    } else {
-      activateLightMode()
-      saveToLocal.set('theme', 'light', 2)
-      GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
-    }
-    // handle some cases
-    typeof utterancesTheme === 'function' && utterancesTheme()
-    typeof FB === 'object' && window.loadFBComment()
-    window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
-  }
-/* hometop JS */
-var bywind = {
-    hideTodayCard: function() {
-        document.getElementById("todayCard") && document.getElementById("todayCard").classList.add("hide")
-    }
-}
-$(".topGroup").hover((function() {}
-), (function() {
-        document.getElementById("todayCard").classList.remove("hide"),
-        document.getElementById("todayCard").style.zIndex = 1
-    }
-))
-
 function whenDOMReady() {
 	initEssay();
 };
