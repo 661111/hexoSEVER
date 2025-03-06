008fc1da5f51fd37f3be5a4f090fe3d30328d8a8
661111 <xiaoxin104@outlook.com>
2025-03-06 09:27:39 +0800

    添加部分内容

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,46 @@
+//用到的各个插件
 var gulp = require('gulp');
-
-//Plugins模块获取
-var minifycss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-html-minifier-terser');
 var htmlclean = require('gulp-htmlclean');
+// gulp-tester
+var terser = require('gulp-terser');
+// 压缩js
+gulp.task('compress', async() =>{
+  gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+    .pipe(terser())
+    .pipe(gulp.dest('./public'))
+});
 //压缩css
-gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
-        .pipe(minifycss())
+gulp.task('minify-css', () => {
+    return gulp.src(['./public/**/*.css'])
+        .pipe(cleanCSS({
+            compatibility: 'ie11'
+        }))
         .pipe(gulp.dest('./public'));
 });
 //压缩html
-gulp.task('minify-html', function() {
+gulp.task('minify-html', () => {
     return gulp.src('./public/**/*.html')
         .pipe(htmlclean())
         .pipe(htmlmin({
-            removeComments: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true,
+            removeComments: true, //清除html注释
+            collapseWhitespace: true, //压缩html
+            collapseBooleanAttributes: true,
+            //省略布尔属性的值，例如：<input checked="true"/> ==> <input />
+            removeEmptyAttributes: true,
+            //删除所有空格作属性值，例如：<input id="" /> ==> <input />
+            removeScriptTypeAttributes: true,
+            //删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true,
+            //删除<style>和<link>的 type="text/css"
+            minifyJS: true, //压缩页面 JS
+            minifyCSS: true, //压缩页面 CSS
+            minifyURLs: true  //压缩页面URL
         }))
-
         .pipe(gulp.dest('./public'))
 });
-//压缩js 不压缩min.js
-gulp.task('minify-js', function() {
-    return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-        .pipe(uglify())
-        .pipe(gulp.dest('./public'));
-});
-
-//4.0以后的写法
-// 执行 gulp 命令时执行的任务
-gulp.task('default', gulp.parallel('minify-html', 'minify-css', 'minify-js', function() {
-    // Do something after a, b, and c are finished.
-}));
+// 运行gulp命令时依次执行以下任务
+gulp.task('default', gulp.parallel(
+  'compress', 'minify-css', 'minify-html'
+))
