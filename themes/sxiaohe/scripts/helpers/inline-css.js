ac53dcb235bd298a96f81d93c84dcd792655621c
661111 <xiaoxin104@outlook.com>
2025-03-08 16:03:52 +0800

    小改动:删除引入的index.css采用hexo生成器引入css文件

--- themes/sxiaohe/scripts/helpers/inline-css.js
+++ themes/sxiaohe/scripts/helpers/inline-css.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
-const path = require('path');
+const path = require('path')
 
 hexo.extend.filter.register('after_render:html', function (str, data) {
   // 生成的 CSS 文件路径
   const cssPath = path.join(hexo.public_dir, 'css/index.css');
-
+  console.log('Hexo 公共目录:', hexo.public_dir);
+  console.log('CSS 文件路径:', cssPath);
+  console.log('CSS 文件是否存在:', fs.existsSync(cssPath));
   // 检查 CSS 文件是否存在
   if (fs.existsSync(cssPath)) {
     // 读取 CSS 文件内容
